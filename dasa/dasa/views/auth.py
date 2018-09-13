368c18c30f7a2f93af1bab546966f7bea6a0528d
chris turner <-v>
2018-09-12 18:06:02 -0700

    users can delete their data and account

--- dasa/dasa/views/auth.py
+++ dasa/dasa/views/auth.py
@@ -49,31 +49,10 @@ class AuthAPIView(APIViewSet):
             authenticated = Account.check_credentials(request, data['email'], data['password'])
 
             if authenticated:
-                # import pdb; pdb.set_trace()
                 NLTKOutput.remove(request=request, pk=authenticated.id)
-                # import pdb; pdb.set_trace()
                 Account.remove(request=request, pk=authenticated.id)
                 return Response(json='Account and content deleted', status=204)
             
             return Response(json='Not Authorized', status=401)
 
         return Response(json='Not Found', status=404)
-
-
-
-
-# def destroy(self, request, id=None):
-        # """
-        # """
-        # if not request.authenticated_userid:
-        #     return Response(json='Forbidden', status=403)
-
-        # if not id:
-        #     return Response(json='Not Found', status=404)
-
-        # try:
-        #     WeatherLocation.remove(request=request, pk=id)
-        # except (DataError, AttributeError):
-        #     return Response(json='Not Found', status=404)
-
-        # return Response(status=204)
